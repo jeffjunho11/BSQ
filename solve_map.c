ed8aa82a0b81cb3dfb5b062617666988a21ddce6
Junho Oh <junhoh@c1r21s3.42gyeongsan.kr>
2024-02-03 23:39:47 +0900

    jeffoh

--- solve_map.c
+++ solve_map.c
@@ -3,22 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   solve_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: junmin <junmin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
+/*   By: junhoh <junhoh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 20:05:15 by junhoh            #+#    #+#             */
-/*   Updated: 2024/02/03 23:11:34 by junmin           ###   ########.fr       */
+/*   Updated: 2024/02/03 23:33:01 by junhoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdlib.h>
 #include "map.h"
 
-/*
-int	valid_check()
-{
-	
-}
-*/
 int	min(int a, int b)
 {
 	if (a < b)
@@ -85,19 +79,23 @@ t_pos	*solve_map(t_map *map, t_pos *max)
 	int	i;
 	int	j;
 	int	min_check;
-	int	**dp;
+	int	dp[100][100];
 
-	dp = (int **)malloc(sizeof(int *) * (map->height + 1));
+	//dp = (int **)malloc(sizeof(int *) * (map->height + 1));
 	while (i < map->height)
 	{
 		j = 0;
-		dp[i] = (int *)malloc(sizeof(int) * (map->width + 1));
+		//dp[i] = (int *)malloc(sizeof(int) * (map->width + 1));
 		while (j < map->width)
 		{
-			if (*get_data(map, i, j) == obstacle)
+			if (*get_data(map, i, j) != obstacle)
+			{
 				dp[i][j] = 0;
+				printf("%d\n", *get_data(map, i, j));
+			}
 			else
 				dp[i][j] = 1;
+			printf("%d\n", *get_data(map, i, j));
 			if (i == 0 || j == 0)
 				continue ;
 			if (dp[i][j] != 0)
@@ -109,8 +107,22 @@ t_pos	*solve_map(t_map *map, t_pos *max)
 		}
 		i++;
 	}
-	find_max_square(map, max, dp);
-	fill_map(map, max, dp);
+	
+	i = 0;
+	j = 0;
+	while (i < map->height)
+	{
+		j = 0;
+		while (j < map->width)
+		{
+			printf("%d", dp[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+	//find_max_square(map, max, dp);
+	//fill_map(map, max, dp);
 	// memory_free(dp, map);
 	return (max);
 }
